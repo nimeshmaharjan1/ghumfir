ec1a5c9c9ad0863abbb24d7114220a758673e84e
nimeshmaharjan1 <maharjannimesh11@gmail.com>
2022-09-18 23:39:34 +0545

    home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
           <button className="btn-primary-500 mt-7">Plan a Trip</button>
         </div>
       </main>
-      <section className="min-h-screen bg-white  text-black pb-14">
+      <section className="min-h-screen bg-white  text-black">
         <div className=" container flex flex-col gap-4">
           <h3 className="text-center text-4xl uppercase font-bold mt-12 px-12 leading-relaxed ">
             LET’S ENJOY AND TRAVEL WITH YOUR RECOMMENDED COMPANION
@@ -37,9 +37,9 @@ const Home: NextPage = () => {
             We offer variety of tours and destinations to all kind of ghumfirer’s based on the recommended interest. Ghumfir user can enjoy
             exlusive deals and tours along with their matched companions.
           </p>
-          <div className="grid grid-cols-3 place-items-center mt-12 !gap-x-64">
-            <div className="first">
-              <div className="bg-orange-500 p-8">
+          <div className="grid grid-cols-2 place-items-center mt-4 gap-2">
+            <div className="first !h-[540px] w-[250px]">
+              <div className="bg-orange-500 p-8 !h-[470px] w-[386px] ">
                 <h2 className="font-bold text-white text-3xl">Nature Tours</h2>
                 <p className="text-white text-sm mt-3 leading-loose">
                   Our aim is to provide ghumfirers with the tour service of the top quality, and we provide yo find suitable tour by
@@ -50,19 +50,16 @@ const Home: NextPage = () => {
                 <button className="bg-white mt-6 w-80 h-12 rounded-lg hover:outline-orange-200 hover:shadow-lg">Learn More</button>
               </div>
             </div>
-            <div className="second">
-              <Image src="/images/first.png" height={800} width={800} alt="hero"></Image>
-            </div>
-            <div className="third">
-              <div className="bg-blue-500 p-8">
-                <h2 className="font-bold text-white text-3xl">Nature Tours</h2>
+            <div className="third !h-[600px] w-[386px]">
+              <div className="bg-blue-500 p-8  !h-[440px] w-[386px] ">
+                <h2 className="font-bold text-white text-3xl">Exclusive Deals</h2>
                 <p className="text-white text-sm mt-3 leading-loose">
-                  Our aim is to provide ghumfirers with the tour service of the top quality, and we provide yo find suitable tour by
-                  recommending with the taste of similar users. Our aim is to provide ghumfirers with the tour service of the top quality,
-                  and we provide yo find suitable tour by recommending with the taste of similar users.Our aim is to provide ghumfirers with
-                  the tour service of the top quality, and we provide yo find suitable tour by recommending with the taste of similar users.
+                  Our mission is to match the user with similar interest and taste. All our tours and excursions are available and
+                  recommended accordingly at really affordable prices so you can always pick a great destination. Our mission is to match
+                  the user with similar interest and taste. All our tours and excursions are available and recommended accordingly at really
+                  affordable prices so you can always pick a great destination.
                 </p>
-                <button className="bg-white mt-6 w-80 h-12 rounded-lg hover:outline-orange-200 hover:shadow-lg">Learn More</button>
+                <button className="bg-white mt-6 w-80 h-12 rounded-lg hover:outline-orange-200 hover:shadow-lg">View All</button>
               </div>
             </div>
           </div>
